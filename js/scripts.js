3f096f94b7fdb6a81fa56f22f5e094abd72af911
aleanrocha <ale.oficial.me@gmail.com>
2023-09-22 20:46:25 -0300

    generating custom password

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,16 @@
 const generatePasswordButton = document.querySelector("#generate-password")
 const generatePasswordElement = document.querySelector("#generated-password")
 
+// novas funcionalidades
+const generateOptions = document.querySelector(".generate-options")
+const createPassword = document.querySelector("#generate-pass-option")
+const buttonCopy = document.querySelector("#password > ion-icon")
+const lengthInput = document.querySelector("#ilength")
+const lettersInput = document.querySelector("#letters")
+const numbersInput = document.querySelector("#inumbers")
+const symbolsInput = document.querySelector("#isymbols")
+
+
 // Funções
 
 // letras, numeros e símbolos
@@ -26,16 +36,22 @@ const getSymbol = () => {
   const symbol = "@#$%&()^><?*=+"
   return symbol[Math.floor(Math.random() * symbol.length)]
 }
-
+const dataGenerators = (generators) => {
+  if (lettersInput.checked) generators.push(getLetterLowerCase,getLetterUpperCase)
+  if (numbersInput.checked) generators.push(getNumber)
+  if (symbolsInput.checked) generators.push(getSymbol)
+  if (generators.length === 0) return
+}
 const generatePassword = (getLetterLowerCase, getLetterUpperCase, getNumber, getSymbol) => {
   let password = ""
-  const passwordLenght = 10
-  const generators = [
-    getLetterLowerCase,
-    getLetterUpperCase,
-    getNumber,
-    getSymbol
-  ]
+  // segunda versão
+  const passwordLenght = lengthInput.value
+  const generators = []
+  if (passwordLenght > 20) {
+    alert("Por favor, crie uma senha de até no máximo 20 caracteres.")
+    return
+  }
+  dataGenerators(generators)
   for(let c = 0 ; c < passwordLenght ; c += generators.length) {
     generators.forEach(() => {
       const generatorValue = generators[Math.floor(Math.random() * generators.length)]()
@@ -43,14 +59,15 @@ const generatePassword = (getLetterLowerCase, getLetterUpperCase, getNumber, get
     })
   }
   password = password.slice(0, passwordLenght)
-
   generatePasswordElement.style.display = "block"
   generatePasswordElement.querySelector("h4").textContent = password
 }
 
 // Eventos
 
-generatePasswordButton.addEventListener("click", () => {
+createPassword.addEventListener("click", () => {
   generatePassword(getLetterLowerCase, getLetterUpperCase, getNumber, getSymbol)
 })
+
+generatePasswordButton.addEventListener("click", () => generateOptions.classList.toggle("hide"))
 
